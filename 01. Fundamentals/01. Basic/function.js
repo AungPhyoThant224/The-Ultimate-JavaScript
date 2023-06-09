fd5c6e982dfd00997a80c05548907e4683c50ce6
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-09 14:28:42 +0630

    Type of Functions.

--- 01. Fundamentals/01. Basic/function.js
+++ 01. Fundamentals/01. Basic/function.js
@@ -1,5 +1,16 @@
+// Perform a task
 function greet(firstName, lastName){
     console.log("Hello " + firstName + " " + lastName);
 }
 
 greet('John', 'Smith');
+
+// Calculate a value
+function square(number){
+    return number * number;
+}
+
+console.log(square(2));
+/* In above line console.log() is a function and square(2) is also a function
+    so that is two functions call.
+*/

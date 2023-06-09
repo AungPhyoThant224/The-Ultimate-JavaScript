1395f7d7050f0fefc79271999ac1b01521122c45
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-09 13:16:45 +0630

    Variables.

---
+++ 01. Fundamentals/01. Basic/variables.js
@@ -0,0 +1,11 @@
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningFul
+// Cannot strat with the Number (1name)
+// Cannot contain a Space of hyphen(-)
+// Are cse-sensityive
+
+let firstName = 'Mosh';
+let secondName = "Hamedani";

33252778b1aa2f51efb8711a3442e9b0c17a7cd7
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-09 16:48:32 +0630

    Operators Precedence.

---
+++ 01. Fundamentals/02. Operators/operatorsPrecedence.js
@@ -0,0 +1,13 @@
+/*
+    Operator Precedence
+    1. (),
+    2. /, %,
+    3. *,
+    4. +, -,
+*/
+
+let firstNum = 1 + 2 * 3;
+console.log(firstNum); // res = 7;
+
+let secondNum = (1 + 2) * 3;
+console.log(secondNum); // res = 9;

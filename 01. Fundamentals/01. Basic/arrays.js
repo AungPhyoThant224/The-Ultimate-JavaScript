efe21b8d7ccec02aba8b3f17a38a49e1e422161a
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-09 14:03:35 +0630

    Arrays.

---
+++ 01. Fundamentals/01. Basic/arrays.js
@@ -0,0 +1,6 @@
+let selectedColor = ['Red', 'Blue'];
+selectedColor[2] = 1;
+console.log(selectedColor.length)
+
+/* NOTE: The length of the array and the type of items in the array are 
+dynamic. */

e0d031481f4b6ab7c878dd4110892cb66c9f536a
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-09 13:51:27 +0630

    Objects.

---
+++ 01. Fundamentals/01. Basic/objects.js
@@ -0,0 +1,12 @@
+let person = {
+    name: 'Mosh',
+    age: 30
+}
+
+//Dot Notation
+person.name = 'Mary';
+console.log(person.name);
+
+//Bracket Notation
+person['age'] = 25;
+console.log(person['age']);

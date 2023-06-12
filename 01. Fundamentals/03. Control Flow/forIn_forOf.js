cfb41c9cdc5aab94e08feb07b79200e7fa59f87a
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-12 10:39:01 +0630

    For Of Loop.

--- 01. Fundamentals/03. Control Flow/forIn_forOf.js
+++ 01. Fundamentals/03. Control Flow/forIn_forOf.js
@@ -1,5 +1,9 @@
 /**
  * Variable key will get the keys of person objcect.
+ * 
+ * Conclusion of for..in and for..of loops
+ * i. Use for..in loop to iterate over the properties of object.
+ * ii. Use for..of to iterate over elements of items in array.
  */
 let person = {
     name: 'Mosh',
@@ -15,4 +19,12 @@ for(let key in person){
 let colors = ["red", "green", "blue"];
 for(let index in colors){
     console.log(index, colors[index]);
+}
+
+/**
+ * Variable fruit will get the each items of fruits array.
+ */
+let frutis = ["apple", "orange", "grape"];
+for(let fruit of frutis){
+    console.log(fruit);
 }

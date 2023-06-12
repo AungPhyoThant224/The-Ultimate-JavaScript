d4e10a346e2f0644666e8b6aa086125ed3b7ddae
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-12 10:02:56 +0630

    Do While.

---
+++ 01. Fundamentals/03. Control Flow/doWhile.js
@@ -0,0 +1,14 @@
+/**
+ * Expression;
+ * do { statement }
+ * while ( condition );
+ * Difference from while loop is, always execute at least once even the
+ * condition is false.
+ */
+
+let i = 9;
+
+do{
+    console.log(i);
+}
+while(i <= 5);

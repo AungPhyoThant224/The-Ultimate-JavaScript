ea030af88313da9874927a4e5ed3405560d387d4
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-12 14:55:17 +0630

    Basic.

---
+++ 01. Fundamentals/04. Objects/Basic.js
@@ -0,0 +1,15 @@
+let obj = {
+  name: "a",
+  radius: 1,
+  isVisible: true,
+  arrays: ["a", "b"],
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("Draw");
+  },
+};
+
+obj.draw();

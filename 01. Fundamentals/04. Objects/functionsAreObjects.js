d85e1ce46b717bb8de2a3b7188a70e5b2b1a477b
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-12 15:59:47 +0630

    Functions are Objects.

---
+++ 01. Fundamentals/04. Objects/functionsAreObjects.js
@@ -0,0 +1,20 @@
+function CreateCircle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("Draw");
+  };
+}
+const circle = new CreateCircle(1);
+// CreateCircle.call({}, 1); //have multiple parameter 1, 2, 3
+// CreateCircle.apply({}, 1); //have multiple parameter [1, 2, 3]
+// Both are same as new CreateCircle(1);
+
+const Circle1 = new Function(
+  "radius",
+  `this.radius = radius;
+    this.draw = function () {
+        console.log("Draw");
+    };`
+);
+
+const another = Circle1;

8bc5f7f12945085a5ff9b34798cd6d9bb7540189
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-12 11:07:56 +0630

    Exercise: Landscape or Portriate.

--- 01. Fundamentals/03. Control Flow/exercise.js
+++ 01. Fundamentals/03. Control Flow/exercise.js
@@ -1,5 +1,11 @@
 //Max of Two Numbers.
-console.log(maxNum(1, 2));
-function maxNum(firstNum, secondNum){
-    return (firstNum > secondNum) ? firstNum : secondNum;
+// console.log(maxNum(1, 2));
+// function maxNum(firstNum, secondNum){
+//     return (firstNum > secondNum) ? firstNum : secondNum;
+// }
+
+//Landscape or Portriate
+console.log(isLandscape(600, 300));
+function isLandscape(width, height) {
+    return (width > height);
 }

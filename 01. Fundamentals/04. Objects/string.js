899286ff13ef5d2175069ddde363628e9d956932
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 09:58:32 +0630

    String.

---
+++ 01. Fundamentals/04. Objects/string.js
@@ -0,0 +1,40 @@
+let message = "This is my first message.";
+
+/**
+ * Return the length of String
+ */
+console.log(message.length);
+
+/**
+ * Check 'first' is included or not
+ */
+console.log(message.includes("first"));
+
+/**
+ * Return the index of 'is'
+ */
+console.log(message.indexOf("is"));
+
+/**
+ * Replace the first with second in the String
+ */
+console.log(message.replace("first", "second"));
+
+/**
+ * Remove the white spaces
+ */
+console.log(message.trim());
+
+/**
+ * Split the String form ' ' and return an array
+ */
+console.log(message.split(" "));
+
+/**
+ * Escspe Notation
+ */
+let escape = "He can't move.";
+
+/**
+ * For more informations visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+ */

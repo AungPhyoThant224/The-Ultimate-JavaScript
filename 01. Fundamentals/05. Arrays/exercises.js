5f2980c35636c4e4ea04975fd32b32876f9afb55
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 21:41:00 +0630

    Exercises.

--- 01. Fundamentals/05. Arrays/exercises.js
+++ 01. Fundamentals/05. Arrays/exercises.js
@@ -1,57 +1,57 @@
 /**
  * Array from Range
  */
-// console.log(arrayFromRange(-10, -4));
-// function arrayFromRange(min, max) {
-//   let arr = [];
-//   while (min <= max) {
-//     arr.push(min);
-//     min++;
-//   }
-//   return arr;
-// }
+console.log(arrayFromRange(-10, -4));
+function arrayFromRange(min, max) {
+  let arr = [];
+  while (min <= max) {
+    arr.push(min);
+    min++;
+  }
+  return arr;
+}
 
 /**
  * Includes
  */
-// console.log(includes([1, 2, 3, 4], 5));
-// function includes(array, search) {
-//   return array.some((value) => value === search);
-// }
+console.log(includes([1, 2, 3, 4], 5));
+function includes(array, search) {
+  return array.some((value) => value === search);
+}
 
 /**
  * Except
  */
-// console.log(except([1, 2, 3, 4, 1, 1], [1, 2]));
-// function except(numbers, excluded) {
-//   const newNumbers = [];
-//   numbers.map((value) => {
-//     if (!excluded.includes(value)) {
-//       newNumbers.push(value);
-//     }
-//   });
-//   return newNumbers;
-// }
+console.log(except([1, 2, 3, 4, 1, 1], [1, 2]));
+function except(numbers, excluded) {
+  const newNumbers = [];
+  numbers.map((value) => {
+    if (!excluded.includes(value)) {
+      newNumbers.push(value);
+    }
+  });
+  return newNumbers;
+}
 
 /**
  * Moving an Element
  */
-// let array = [1, 2, 3, 4];
-// console.log(move(array, 2, -1));
+let array = [1, 2, 3, 4];
+console.log(move(array, 2, -1));
 
 //-------------Mosh Solution-----------
-// function move(array, index, offset) {
-//   let position = index + offset;
-//   if (!isValidOffset(array, position)) {
-//     console.error("Invalid Offset");
-//     return;
-//   }
+function move(array, index, offset) {
+  let position = index + offset;
+  if (!isValidOffset(array, position)) {
+    console.error("Invalid Offset");
+    return;
+  }
 
-//   const output = [...array];
-//   const element = output.splice(index, 1)[0];
-//   output.splice(position, 0, element);
-//   return output;
-// }
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
+  return output;
+}
 
 //-------------My Solution--------------
 // function move(array, index, offset) {
@@ -78,40 +78,39 @@
 //   return newArr;
 // }
 
-// function isValidOffset(array, position) {
-//   if (position < 0 || position > array.length - 1) {
-//     return false;
-//   }
-//   return true;
-// }
+function isValidOffset(array, position) {
+  if (position < 0 || position > array.length - 1) {
+    return false;
+  }
+  return true;
+}
 
 /**
  * Count Occurrences
  */
-
-// console.log(countOccurrences([1, 2, 3, 4, 1], 1));
-// function countOccurrences(array, searchElement) {
-//   const result = array.reduce((accumulate, current) => {
-//     if (current === searchElement) {
-//       accumulate++;
-//     }
-//     return accumulate;
-//   }, 0);
-//   return result;
-// }
+console.log(countOccurrences([1, 2, 3, 4, 1], 1));
+function countOccurrences(array, searchElement) {
+  const result = array.reduce((accumulate, current) => {
+    if (current === searchElement) {
+      accumulate++;
+    }
+    return accumulate;
+  }, 0);
+  return result;
+}
 
 /**
  * Get Max
  */
-// console.log(getMax([1, 2, 13, 4, 10, 20]));
-// function getMax(array) {
-//   if (array.length === 0) {
-//     return;
-//   }
-//   let max = array.reduce(
-//     (accumulator, current) => (current > accumulator) ? current : accumulator);
-//   return max;
-// }
+console.log(getMax([1, 2, 13, 4, 10, 20]));
+function getMax(array) {
+  if (array.length === 0) {
+    return;
+  }
+  let max = array.reduce(
+    (accumulator, current) => (current > accumulator) ? current : accumulator);
+  return max;
+}
 
 /**
  * Movies

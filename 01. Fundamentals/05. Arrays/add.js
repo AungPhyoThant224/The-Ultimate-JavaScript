7035b241b95aa4cd61fb95f6a754dfd366d3d063
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 11:41:32 +0630

    Add Element.

---
+++ 01. Fundamentals/05. Arrays/add.js
@@ -0,0 +1,18 @@
+let nums = [4, 5];
+
+/**
+ * Add at the End
+ */
+nums.push(6, 7);
+
+/**
+ * Add at the Start
+ */
+nums.unshift(1, 2);
+
+/**
+ * Add at in the Middle
+ */
+nums.splice(2, 0, 3);
+
+console.log(nums);

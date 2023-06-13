1b512133d7d099630a6fecbb8828ebcd01ed6c62
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 16:36:34 +0630

    Exercise : Except.

--- 01. Fundamentals/05. Arrays/exercises.js
+++ 01. Fundamentals/05. Arrays/exercises.js
@@ -14,7 +14,21 @@
 /**
  * Includes
  */
-console.log(includes([1, 2, 3, 4], 5));
-function includes(array, search) {
-  return array.some((value) => value === search);
+// console.log(includes([1, 2, 3, 4], 5));
+// function includes(array, search) {
+//   return array.some((value) => value === search);
+// }
+
+/**
+ * Except
+ */
+console.log(except([1, 2, 3, 4, 1, 1], [1, 2]));
+function except(numbers, excluded) {
+  const newNumbers = [];
+  numbers.map((value) => {
+    if (!excluded.includes(value)) {
+      newNumbers.push(value);
+    }
+  });
+  return newNumbers;
 }

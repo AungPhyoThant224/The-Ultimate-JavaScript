e8d5fc2db0986776d2388aa5235f929099ffbd25
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 20:26:13 +0630

    Exercise : Moving an Element.

--- 01. Fundamentals/05. Arrays/exercises.js
+++ 01. Fundamentals/05. Arrays/exercises.js
@@ -22,13 +22,65 @@
 /**
  * Except
  */
-console.log(except([1, 2, 3, 4, 1, 1], [1, 2]));
-function except(numbers, excluded) {
-  const newNumbers = [];
-  numbers.map((value) => {
-    if (!excluded.includes(value)) {
-      newNumbers.push(value);
-    }
-  });
-  return newNumbers;
+// console.log(except([1, 2, 3, 4, 1, 1], [1, 2]));
+// function except(numbers, excluded) {
+//   const newNumbers = [];
+//   numbers.map((value) => {
+//     if (!excluded.includes(value)) {
+//       newNumbers.push(value);
+//     }
+//   });
+//   return newNumbers;
+// }
+
+/**
+ * Moving an Element
+ */
+let array = [1, 2, 3, 4];
+console.log(move(array, 2, -1));
+
+//-------------Mosh Solution-----------
+function move(array, index, offset) {
+  let position = index + offset;
+  if (!isValidOffset(array, position)) {
+    console.error("Invalid Offset");
+    return;
+  }
+
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
+  return output;
+}
+
+//-------------My Solution--------------
+// function move(array, index, offset) {
+//   let position = index + offset;
+//   if (!isValidOffset(array, position)) {
+//     console.error("Invalid Offset");
+//     return;
+//   }
+
+//   let newArr = [...array];
+//   if (index < position) {
+//     for (let i = index; i < position; i++) {
+//       var temp = newArr[i];
+//       newArr[i] = newArr[i + 1];
+//       newArr[i + 1] = temp;
+//     }
+//   } else if (index > position) {
+//     for (let i = index; i > position; i--) {
+//       var temp = newArr[i];
+//       newArr[i] = newArr[i - 1];
+//       newArr[i - 1] = temp;
+//     }
+//   }
+//   return newArr;
+// }
+
+function isValidOffset(array, position) {
+  if (position < 0 || position > array.length - 1) {
+    return false;
+  }
+  return true;
 }

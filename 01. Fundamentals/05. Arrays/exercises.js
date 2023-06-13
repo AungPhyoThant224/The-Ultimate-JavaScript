5faf6fe93bb093392116a951d4a2d3d5808c66b5
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 16:27:31 +0630

    Exercise : Includes.

--- 01. Fundamentals/05. Arrays/exercises.js
+++ 01. Fundamentals/05. Arrays/exercises.js
@@ -1,14 +1,20 @@
 /**
  * Array from Range
  */
+// console.log(arrayFromRange(-10, -4));
+// function arrayFromRange(min, max) {
+//   let arr = [];
+//   while (min <= max) {
+//     arr.push(min);
+//     min++;
+//   }
+//   return arr;
+// }
 
-console.log(arrayFromRange(-10, -4));
-
-function arrayFromRange(min, max) {
-  let arr = [];
-  while (min <= max) {
-    arr.push(min);
-    min++;
-  }
-  return arr;
+/**
+ * Includes
+ */
+console.log(includes([1, 2, 3, 4], 5));
+function includes(array, search) {
+  return array.some((value) => value === search);
 }

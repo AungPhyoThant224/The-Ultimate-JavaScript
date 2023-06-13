5553a1de2d30d1dd20f312305422783174398ba1
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 09:50:24 +0630

    Math.

---
+++ 01. Fundamentals/04. Objects/math.js
@@ -0,0 +1,13 @@
+/**
+ * Give the random number between 1 and 0
+ */
+console.log(Math.random());
+
+/**
+ * Give the max value
+ */
+console.log(Math.max(1, 2, 3, 4));
+
+/**
+ * For more information visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+ */

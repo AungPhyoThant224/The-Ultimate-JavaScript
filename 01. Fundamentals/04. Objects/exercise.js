8b38cddc87d915bd7a3b4e1dd86ce935cce372bb
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 10:44:52 +0630

    Exercise: Factory and Constructor Functions.

--- 01. Fundamentals/04. Objects/exercise.js
+++ 01. Fundamentals/04. Objects/exercise.js
@@ -1,15 +1,36 @@
 /**
  * Address Object
  */
+// const address = {
+//   street: "Myathisar Road",
+//   city: "Mogoke",
+//   zipCode: "05201",
+// };
 
-const address = {
-  street: "Myathisar Road",
-  city: "Mogoke",
-  zipCode: "05201",
-};
-getAddress(address);
-function getAddress(address) {
-  for (let data in address) {
-    console.log(data, address[data]);
-  }
+// function getAddress(address) {
+//   for (let data in address) {
+//     console.log(data, address[data]);
+//   }
+// }
+
+// getAddress(address);
+
+/**
+ * Factory and Constructor Functions
+ */
+function fruitFactory(name, color) {
+  return {
+    name,
+    color,
+  };
 }
+
+function fruitConstructor(name, color) {
+  this.name = name;
+  this.color = color;
+}
+
+const apple = fruitFactory("apple", "red");
+const orange = new fruitConstructor("orange", "orange");
+console.log("fruitFactory", apple);
+console.log(orange);

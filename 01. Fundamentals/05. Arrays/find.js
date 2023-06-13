725720e10b3be18b2208986b54f4f02291200461
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 13:39:24 +0630

    Finding Element (Reference).

--- 01. Fundamentals/05. Arrays/find.js
+++ 01. Fundamentals/05. Arrays/find.js
@@ -1,3 +1,6 @@
+/**
+ * Primitive
+ */
 let arr = [1, 2, 3, 1, 4];
 
 /**
@@ -24,3 +27,25 @@ console.log(arr.includes(5));
  * NOTE: All these methods have the second parameter "Start Index"
  */
 console.log(arr.indexOf(1, 2));
+
+/********************************/
+
+/**
+ * Reference
+ */
+
+let courses = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+];
+
+const course = courses.find(function (course) {
+  return course.name === "b";
+});
+console.log(course);
+
+const courseIndex = courses.findIndex(function (course) {
+  console.log(course.name);
+  return course.name === "b";
+});
+console.log(courseIndex);

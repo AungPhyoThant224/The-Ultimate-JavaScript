1f80fa6c905b465a68b24eee693a4f80c0d61c3c
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 11:26:03 +0630

    Exercise.

--- 01. Fundamentals/04. Objects/exercise.js
+++ 01. Fundamentals/04. Objects/exercise.js
@@ -1,19 +1,19 @@
 /**
  * Address Object
  */
-// const address = {
-//   street: "Myathisar Road",
-//   city: "Mogoke",
-//   zipCode: "05201",
-// };
+const address = {
+  street: "Myathisar Road",
+  city: "Mogoke",
+  zipCode: "05201",
+};
 
-// function getAddress(address) {
-//   for (let data in address) {
-//     console.log(data, address[data]);
-//   }
-// }
+function getAddress(address) {
+  for (let data in address) {
+    console.log(data, address[data]);
+  }
+}
 
-// getAddress(address);
+getAddress(address);
 
 /**
  * Factory and Constructor Functions
@@ -32,64 +32,64 @@ function AddressConstructor(city, street, zipCode) {
   this.zipCode = zipCode;
 }
 
-// const address1 = addressFactory("a", "b", "c");
-// const address2 = new AddressConstructor("a", "b", "c");
-// console.log("addressFactory", apple);
-// console.log(orange);
+const address1 = addressFactory("a", "b", "c");
+const address2 = new AddressConstructor("a", "b", "c");
+console.log("addressFactory", apple);
+console.log(orange);
 
 /**
  * Object Equality
  */
-// const addr = new AddressConstructor("a", "b");
-// const addr1 = new AddressConstructor("a", "b");
-// const addr2 = addr1;
-// console.log(areEqual(addr, addr1));
-// console.log(areSame(addr, addr1));
-// console.log(areSame(addr1, addr2));
+const addr = new AddressConstructor("a", "b");
+const addr1 = new AddressConstructor("a", "b");
+const addr2 = addr1;
+console.log(areEqual(addr, addr1));
+console.log(areSame(addr, addr1));
+console.log(areSame(addr1, addr2));
 
-// function areEqual(address1, address2) {
-//   for (let data in address1) {
-//     if (address1[data] !== address2[data]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+function areEqual(address1, address2) {
+  for (let data in address1) {
+    if (address1[data] !== address2[data]) {
+      return false;
+    }
+  }
+  return true;
+}
 
-// function areSame(address1, address2) {
-//   return address1 === address2;
-// }
+function areSame(address1, address2) {
+  return address1 === address2;
+}
 
 /**
  * Blog Post
  */
-// let blog = {
-//   title: "a",
-//   body: "b",
-//   author: "c",
-//   views: 100,
-//   comment: [
-//     { author: "a", body: "b" },
-//     { author: "b", body: "b" },
-//   ],
-//   isLive: true,
-// };
-// console.log(blog);
+let blog = {
+  title: "a",
+  body: "b",
+  author: "c",
+  views: 100,
+  comment: [
+    { author: "a", body: "b" },
+    { author: "b", body: "b" },
+  ],
+  isLive: true,
+};
+console.log(blog);
 
 /**
  * Constructor function
  */
-// let post = new Post("a", "b", "c");
-// console.log(post);
+let post = new Post("a", "b", "c");
+console.log(post);
 
-// function Post(title, body, author) {
-//   this.title = title;
-//   this.body = body;
-//   this.author = author;
-//   this.view = 0;
-//   this.comment = [];
-//   this.isLive = false;
-// }
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.view = 0;
+  this.comment = [];
+  this.isLive = false;
+}
 
 /**
  * Price Range

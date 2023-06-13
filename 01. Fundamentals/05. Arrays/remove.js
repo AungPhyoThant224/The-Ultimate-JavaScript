f074dfde8b19b311fafd164ad3075eb684bbb84a
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 13:55:57 +0630

    Remove Elements.

---
+++ 01. Fundamentals/05. Arrays/remove.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 3, 4, 5];
+
+/**
+ * Remove the first item of array
+ */
+arr.pop();
+
+/**
+ * Remove the last item of array
+ */
+arr.shift();
+
+/**
+ * Remove somewhere in the middle of array
+ * 1 = index
+ * 2 = delete count
+ */
+arr.splice(1, 2);
+
+console.log(arr);

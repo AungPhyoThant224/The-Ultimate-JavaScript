22db1ec8316b48ee234c82904c481c518df36258
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-13 10:13:10 +0630

    Template Literals.

---
+++ 01. Fundamentals/04. Objects/templateLiterals.js
@@ -0,0 +1,14 @@
+let name = "John";
+let message = `Hi ${name},
+
+Thank you for joining my mailing list.
+
+Regards,
+Mosh.`;
+
+console.log(message);
+
+/**
+ * We can add any expression with ${}
+ * and also call a function that return a value.
+ */

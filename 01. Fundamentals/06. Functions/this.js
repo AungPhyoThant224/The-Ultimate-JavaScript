c0af9d45c0e474ee055c6fa9606228cfd2e87e0d
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-14 15:18:39 +0630

    The This Keyword.

---
+++ 01. Fundamentals/06. Functions/this.js
@@ -0,0 +1,49 @@
+/**
+ * 1. method -> object
+ * 2. function -> global (window, global)
+ *    With the regular function this references the global object.
+ *    But if you called the function with new operator this will reference
+ *    the new empty object.
+ */
+
+/**
+ * Example 1
+ */
+const video = {
+    title: 'a',
+    play() {
+        console.log(this);
+    }
+}
+video.play();
+
+/**
+ * Example 2
+ */
+const tags = {
+    title: 'a',
+    tags: ['a', 'b', 'c'],
+    getTags() {
+        this.tags.forEach((tag) => {
+            console.log(this.title, tag);
+        }, this)
+    }
+}
+tags.getTags();
+
+/**
+ * Example 3
+ */
+function videoPlay() {
+    console.log(this);
+}
+videoPlay();
+
+/**
+ * Example 3
+ */
+function VideoPlay(title) {
+    this.title = title;
+    console.log(this);
+}
+let play = new VideoPlay('b');

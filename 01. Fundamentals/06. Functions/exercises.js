880db009f02290fbe5f3270962724420ad4c5fdf
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-14 16:10:08 +0630

    Exercises.

--- 01. Fundamentals/06. Functions/exercises.js
+++ 01. Fundamentals/06. Functions/exercises.js
@@ -1,37 +1,37 @@
 /**
  * Sum of Arguments
  */
-// sum(1, 2, [3, 4]);
-// function sum(...args) {
-//     const res = args.reduce((accumulator, current) => {
-//         if (Array.isArray(current)) {
-//             let arrSum = current.reduce((accu, cur) => accu + cur);
-//             return arrSum + accumulator;
-//         }
-//         return accumulator + current;
-//     }, 0)
-//     console.log(res);
-// }
+sum(1, 2, [3, 4]);
+function sum(...args) {
+    const res = args.reduce((accumulator, current) => {
+        if (Array.isArray(current)) {
+            let arrSum = current.reduce((accu, cur) => accu + cur);
+            return arrSum + accumulator;
+        }
+        return accumulator + current;
+    }, 0)
+    console.log(res);
+}
 
-// console.log(sumMosh([1, 2, 3, 4]));
-// function sumMosh(...items) {
-//     if (items.length === 1 && Array.isArray(items[0])) {
-//         items = [...items[0]];
-//     }
-//     return items.reduce((a, b) => a + b);
-// }
+console.log(sumMosh([1, 2, 3, 4]));
+function sumMosh(...items) {
+    if (items.length === 1 && Array.isArray(items[0])) {
+        items = [...items[0]];
+    }
+    return items.reduce((a, b) => a + b);
+}
 
 /**
  * Area of Circle
  */
-// let circle = {
-//     radius: 2,
-//     get area() {
-//         return Math.PI * this.radius * this.radius;
-//     }
-// }
-// console.log(circle.radius);
-// console.log(circle.area);
+let circle = {
+    radius: 2,
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+console.log(circle.radius);
+console.log(circle.area);
 
 /**
  * Error Handling

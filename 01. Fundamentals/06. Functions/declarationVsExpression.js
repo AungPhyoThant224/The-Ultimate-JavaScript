aaa754f9449d89cd3f4aaa7e09bbb0633302c885
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-14 09:21:09 +0630

    Function Declarations Vs Expressions.

---
+++ 01. Fundamentals/06. Functions/declarationVsExpression.js
@@ -0,0 +1,21 @@
+/**
+ * Function Declaration
+ */
+function walk() {
+    console.log('Walk');
+}
+
+/**
+ * Anonymous Function Expression
+ */
+let move = function () {
+    console.log('Move');
+};
+
+/**
+ * Named Function Expression
+ */
+let run = function running() {
+    console.log('Run');
+}
+run();

0ee6761ab7a8a336bcb2544dfe1fa4b4875d7719
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-16 14:20:59 +0630

    Private Members using WeakMap.

---
+++ 02. Advanced Topics/04. ES6 Classes/privateMembersWeakMap.js
@@ -0,0 +1,25 @@
+const _radius = WeakMap();
+const _move = WeakMap();
+
+class Circle {
+    constructor(radius) {
+        /**
+         * Private properties
+         */
+        _radius.set(this, radius);
+
+        /**
+         * Private methods
+         */
+        // _move.set(this, function () {}) // this == undefinded
+        _move.set(this, () => console.log(this, 'move')); // this == Circle
+    }
+
+    draw() {
+        /**
+         * Access private members
+         */
+        console.log(_radius.get(this));
+        _move.get(this)();
+    }
+}
